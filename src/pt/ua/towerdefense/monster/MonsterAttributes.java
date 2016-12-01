603218ded75c115bf235a24d113f45b537bb80e0
Eduardo Sousa <eduardosousa@ua.pt>
2016-12-01 19:11:58 +0000

    Adding documentation for MonsterAttributes

--- src/pt/ua/towerdefense/monster/MonsterAttributes.java
+++ src/pt/ua/towerdefense/monster/MonsterAttributes.java
@@ -1,9 +1,24 @@
 package pt.ua.towerdefense.monster;
 
+/**
+ * Class that represents the Monsters attributes, like: health, speed.
+ * The objects of this class are immutable.
+ *
+ * @author Eduardo Sousa
+ */
 public class MonsterAttributes {
+    /* Health of the monster */
     private final int health;
+
+    /* Speed to which the monster moves */
     private final int speed;
 
+    /**
+     * Constructor for objects of the type MonsterAttributes.
+     *
+     * @param health health of the monster
+     * @param speed speed of the monster
+     */
     public MonsterAttributes(int health, int speed) {
         assert health > 0;
         assert speed > 0;
@@ -12,10 +27,20 @@ public class MonsterAttributes {
         this.speed = speed;
     }
 
+    /**
+     * Getter for the monster's health.
+     *
+     * @return a positive integer representing the monster's health.
+     */
     public int getHealth() {
         return health;
     }
 
+    /**
+     * Getter for the monster's speed.
+     *
+     * @return a positive integer representing the monster's speed.
+     */
     public int getSpeed() {
         return speed;
     }
