075da5b9f7cfcacad406ae44f8646ad5da4ee705
Eduardo Sousa <eduardosousa@ua.pt>
2016-12-01 18:33:08 +0000

    Adding documentation to Position

--- src/pt/ua/towerdefense/Position.java
+++ src/pt/ua/towerdefense/Position.java
@@ -1,11 +1,29 @@
 package pt.ua.towerdefense;
 
+/**
+ * Class that represents the position of an object in the
+ * map. Objects of this class are immutable.
+ *
+ * <b>Note:</b>  it's required to set the map before creating
+ * objects of this class.
+ *
+ * @author Eduardo Sousa
+ */
 public class Position {
+    /* Map to which the position is relative to */
     private static TerrainMap map;
 
+    /* Coordinate in the XX axis */
     private final int coordinateX;
+
+    /* Coordinate in the YY axis */
     private final int coordinateY;
 
+    /**
+     * Sets the map to which the position is relative to.
+     *
+     * @param map map for where all position will be relative to.
+     */
     public static void setMap(TerrainMap map) {
         assert Position.map == null;
         assert map != null;
@@ -15,6 +33,12 @@ public class Position {
         assert Position.map != null;
     }
 
+    /**
+     * Constructor for the Position object.
+     *
+     * @param coordinateX coordinate in the XX axis.
+     * @param coordinateY coordinate in the YY axis.
+     */
     public Position(int coordinateX, int coordinateY) {
         assert Position.map != null;
         assert coordinateX >= 0;
@@ -26,10 +50,20 @@ public class Position {
         this.coordinateY = coordinateY;
     }
 
+    /**
+     * Getter for the coordinate in the XX axis.
+     *
+     * @return coordinate in the XX axis.
+     */
     public int getCoordinateX() {
         return coordinateX;
     }
 
+    /**
+     * Getter for the coordinate in the YY axis.
+     *
+     * @return coordinate in the YY axis.
+     */
     public int getCoordinateY() {
         return coordinateY;
     }
