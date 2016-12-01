31b9cae5dd0d38dbe6628e7cbb775eaf4d86bac5
Eduardo Sousa <eduardosousa@ua.pt>
2016-12-01 19:21:28 +0000

    Adding documentation to Monster

--- src/pt/ua/towerdefense/monster/Monster.java
+++ src/pt/ua/towerdefense/monster/Monster.java
@@ -6,14 +6,35 @@ import pt.ua.towerdefense.Position;
 import pt.ua.towerdefense.RotationWise;
 import pt.ua.towerdefense.TerrainMap;
 
+/**
+ * Class that represents Monsters.
+ *
+ * @author Eduardo Sousa
+ */
 public class Monster extends CThread {
+    /* Monster's attributes. Defines speed and health */
     private final MonsterAttributes attributes;
+
+    /* Defines the map where the monster is */
     private final TerrainMap map;
 
+    /* Health that the monster still has */
     private int remainingHealth;
+
+    /* Position where the monster is at */
     private Position actualPosition;
+
+    /* Heading of the monster */
     private Direction direction;
 
+    /**
+     * Constructor for the Monsters object.
+     *
+     * @param attributes attributes that define the monster's characteristics.
+     * @param map map where the monster is.
+     * @param actualPosition position where the object must start.
+     * @param direction heading of the monster.
+     */
     public Monster(MonsterAttributes attributes, TerrainMap map, Position actualPosition, Direction direction) {
         super();
 
@@ -42,6 +63,11 @@ public class Monster extends CThread {
 
     }
 
+    /**
+     * Checks if there is a road to the left side of the monster.
+     *
+     * @return true if there is a road at his left.
+     */
     private boolean isRoadAvailableLeft() {
         assert this.direction != null;
         assert !this.direction.isSubdirection();
@@ -74,6 +100,11 @@ public class Monster extends CThread {
                 this.map.isPositionAvailable(new Position(x, y));
     }
 
+    /**
+     * Checks if there is a road to the right side of the monster.
+     *
+     * @return true if there is a road at his right.
+     */
     private boolean isRoadAvailableRight() {
         assert this.direction != null;
         assert !this.direction.isSubdirection();
@@ -106,6 +137,11 @@ public class Monster extends CThread {
                 this.map.isPositionAvailable(new Position(x, y));
     }
 
+    /**
+     * Checks if there is a road to the front side of the monster.
+     *
+     * @return true if there is a road at his front.
+     */
     private boolean isRoadAvailableCenter() {
         assert this.direction != null;
         assert !this.direction.isSubdirection();
@@ -138,6 +174,10 @@ public class Monster extends CThread {
                 this.map.isPositionAvailable(new Position(x, y));
     }
 
+    /**
+     * Moves the monster to the tile in front of him.<br>
+     * Blocks while the monster is moving.
+     */
     private void moveInFront() {
         assert this.direction != null;
         assert !this.direction.isSubdirection();
@@ -179,6 +219,12 @@ public class Monster extends CThread {
         assert this.map.isPositionInPath(this.actualPosition);
     }
 
+    /**
+     * Rotates the monster to the desired heading.<br>
+     * Blocks while rotating.
+     *
+     * @param rotation rotation direction (clockwise or counterclockwise).
+     */
     private void rotate(RotationWise rotation) {
         assert this.direction != null;
         assert !this.direction.isSubdirection();
@@ -215,6 +261,11 @@ public class Monster extends CThread {
         assert !this.direction.isSubdirection();
     }
 
+    /**
+     * Function to decrese the amount of health of a monster when it is shot by a tower.
+     *
+     * @param damage damage inflicted by the shot.
+     */
     public void gotShot(int damage) {
         assert damage >= 0;
         assert this.remainingHealth > 0;
