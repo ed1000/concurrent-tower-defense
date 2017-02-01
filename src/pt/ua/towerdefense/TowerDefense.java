947f31aca61e393b9f7fd7b836cda94f354842d6
Eduardo Sousa <eduardosousa@ua.pt>
2017-02-01 13:14:31 +0000

    Added parametrization

--- src/pt/ua/towerdefense/TowerDefense.java
+++ src/pt/ua/towerdefense/TowerDefense.java
@@ -14,23 +14,113 @@ import pt.ua.towerdefense.towers.TowerAttributes;
 import pt.ua.towerdefense.world.Position;
 import pt.ua.towerdefense.world.WorldState;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class TowerDefense {
+    private static int numberOfMonsters;
+    private static Position begin;
+    private static Position end;
+    private static List<Position> towers = new ArrayList<>();
+    private static List<String> map = new ArrayList<>();
+
     public static void main(String[] args) throws InterruptedException {
+        if(args.length != 2) {
+            System.out.println("Required arguments: <number_of_monsters> <map_file>.");
+            System.exit(-1);
+        }
+
+        numberOfMonsters = Integer.decode(args[0]);
+
+        if(numberOfMonsters <= 0) {
+            System.out.println("You must pass a positive number of monsters.");
+            System.exit(-1);
+        }
+
+        Scanner sc = new Scanner(args[1]);
+
+        while (sc.hasNextLine()) {
+            map.add(sc.nextLine().toUpperCase());
+        }
+
+        sc.close();
+
+        if(map.isEmpty()) {
+            System.out.println("Map must not be empty.");
+            System.exit(-1);
+        }
+
+        processMap();
+
+        if(begin == null || end == null) {
+            System.out.println("One begin and end cell must be defined in the map.");
+            System.exit(-1);
+        }
+
         WorldState state = initializeWorldState();
         MonsterAttributes monsterAttributes = initializeMonsterAttributes();
         TowerAttributes towerAttributes = initializeTowerAttributes();
         Metronome metronome = initializeMetronome();
 
-        Tower tower = new Tower(state, metronome, new Position(4, 0), towerAttributes);
-        tower.start();
-        tower = new Tower(state, metronome, new Position(4, 4), towerAttributes);
-        tower.start();
+        for(Position pos : towers) {
+            Tower tower = new Tower(state, metronome, pos, towerAttributes);
+            tower.start();
+        }
 
-        for(int i = 0; i < 25; i++) {
+        for(int i = 0; i < numberOfMonsters; i++) {
             Monster monster = new Monster(monsterAttributes, state, metronome, state.getPathBeginning(), Direction.EAST);
             monster.start();
         }
 
+    }
+
+    private static void processMap() {
+        List<String> processedMap = new ArrayList<>();
+
+        for(int y = 0; y < map.size(); y++) {
+            StringBuilder builder = new StringBuilder();
+
+            for(int x = 0; x < map.get(y).length(); x++) {
+                switch (map.get(y).charAt(x)) {
+                    case 'B':
+                        if(begin != null) {
+                            System.out.println("Map must only contain one begin cell.");
+                            System.exit(-1);
+                        } else {
+                            begin = new Position(x, y);
+                        }
+                        builder.append('X');
+                        break;
+                    case 'E':
+                        if(end != null) {
+                            System.out.println("Map must only contain one end cell.");
+                            System.exit(-1);
+                        } else {
+                            end = new Position(x, y);
+                        }
+                        builder.append('X');
+                        break;
+                    case 'T':
+                        towers.add(new Position(x, y));
+                        builder.append('#');
+                        break;
+                    case 'X':
+                        builder.append('X');
+                        break;
+                    case '#':
+                        builder.append('#');
+                        break;
+                    default:
+                        System.out.println("Unrecognized symbol in map definition. Allowed symbols: B, E, T, X, #.");
+                        System.exit(-1);
+                }
+            }
+
+            processedMap.add(builder.toString());
+        }
+
+        map = processedMap;
     }
 
     private static WorldState initializeWorldState() {
